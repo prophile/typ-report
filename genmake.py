8036cb897a129eeed22411e32c4b3aacdea0e026
Alastair Lynn <arplynn@gmail.com>
2014-03-13 10:09:17 +0000

    Add planning stub section

--- genmake.py
+++ genmake.py
@@ -2,9 +2,14 @@ import re
 import sys
 
 pattern = re.compile('^\\\\include\\{build/(.*)\\}$')
+patternNon = re.compile('^\\\\include\\{(.*)\\}$')
 
 for line in sys.stdin:
     match = pattern.match(line.strip())
     if match:
         print("report.pdf: build/{0}.tex".format(match.group(1)))
+    else:
+        match = patternNon.match(line.strip())
+        if match:
+            print("report.pdf: {0}.tex".format(match.group(1)))
 
